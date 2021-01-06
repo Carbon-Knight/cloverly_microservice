7ccd7c07494a591e515109d4c4c1902523a1fe4f
Sean Steel <seanrsteel@gmail.com>
2021-01-06 07:37:07 -0700

    Setup service tests and create service model
    Co-authored-by: Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
    Co-authored-by: Jonathan Wilson <53539820+Jonathan-M-Wilson@users.noreply.github.com>
    Co-authored-by: Joshua Carey <59512644+jdcarey128@users.noreply.github.com>

--- spec/service/clovery_service_spec.rb
+++ spec/service/clovery_service_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
 
+RSpec.describe 'Cloverly Service' do
+  it 'will return carbon data' do
+    distance = 55
+    fuel_efficiency = 25
+    response = CloverlyService.carbon_data(distance, fuel_efficiency)
+
+    expect(response).to be_a(Hash)
+  end
+end
