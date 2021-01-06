dacffbc5b100e7103e34453393988152566ddad0
Sean Steel <seanrsteel@gmail.com>
2021-01-06 08:12:39 -0700

    Increase testing for cloverly service
    Co-authored-by: Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
    Co-authored-by: Jonathan Wilson <53539820+Jonathan-M-Wilson@users.noreply.github.com>
    Co-authored-by: Joshua Carey <59512644+jdcarey128@users.noreply.github.com>

--- lib/cloverly_service.rb
+++ lib/cloverly_service.rb
@@ -6,14 +6,15 @@ class CloverlyService
   def self.carbon_data(trip_distance, fuel_efficiency)
     carbon_response = conn.post do |req|
       req.url '/2019-03-beta/estimates/vehicle'
-      req.body = '{"distance":{"value":55,"units":"km"},"fuel_efficiency":{"value":25,"units":"mpg","of":"gasoline"}}'
+      req.body = '{"distance":{"value":"#{trip_distance}","units":"km"},"fuel_efficiency":{"value":"#{fuel_efficiency}","units":"mpg","of":"gasoline"}}'
       req.headers['Content-Type'] = 'application/json'
       req.headers['Authorization'] = "Bearer public_key:#{ENV['CLOVERY_PUBLIC_KEY']}"
     end
+    require 'pry', binding.pry
     parse_it(carbon_response)
   end
 
-  private 
+  private
 
   def self.conn
     Faraday.new(:url => 'https://api.cloverly.com')
--- spec/service/clovery_service_spec.rb
+++ spec/service/clovery_service_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe 'Cloverly Service' do
     response = CloverlyService.carbon_data(distance, fuel_efficiency)
 
     expect(response).to be_a(Hash)
+    expect(response).to have_key('slug')
+    expect(response).to have_key('equivalent_carbon_in_kg')
+    expect(response).to have_key('cost')
+    expect(response['cost']).to be_a(Hash)
+    expect(response['cost']).to have_key('currency')
+    expect(response['cost']).to have_key('total')
+
+    expect(response['equivalent_carbon_in_kg']).to be_a(Float)
+    expect(response['cost']['currency']).to be_a(String)
+    expect(response['cost']['total']).to be_a(String)
   end
 end
