0de0b32fc05de18c4ab0efd923b4cdeae7bbba88
Curtis Bartell <cbartell980@gmail.com>
2021-01-06 18:57:43 -0700

    Refactor CloverlyService
    Co-authored-by: Sean Steel <65255478+s-steel@users.noreply.github.com>

--- lib/services/cloverly_service.rb
+++ lib/services/cloverly_service.rb
@@ -7,19 +7,7 @@ class CloverlyService
     def carbon_data(trip_distance, fuel_efficiency)
       carbon_response = conn.post do |req|
         req.url '/2019-03-beta/estimates/vehicle'
-        req.body = JSON.generate(
-          distance: {
-            value: trip_distance.to_s,
-            units: 'km'
-          },
-          fuel_efficiency: {
-            value: fuel_efficiency.to_s,
-            units: 'mpg',
-            of: 'gasoline'
-          }
-        )
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['Authorization'] = "Bearer public_key:#{ENV['CLOVERY_PUBLIC_KEY']}"
+        req.body = request_body(trip_distance, fuel_efficiency)
       end
       parse_it(carbon_response)
     end
@@ -27,11 +15,36 @@ class CloverlyService
     private
 
     def conn
-      Faraday.new(url: 'https://api.cloverly.com')
+      Faraday.new(url: 'https://api.cloverly.com') do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['Authorization'] = "Bearer public_key:#{ENV['CLOVERY_PUBLIC_KEY']}"
+      end
     end
 
     def parse_it(data)
       JSON.parse(data.body, sybolize_names: true)
+    end
+
+    def request_body(trip_distance, fuel_efficiency)
+      JSON.generate(
+        distance: format_distance(trip_distance),
+        fuel_efficiency: format_fuel_efficiency(fuel_efficiency)
+      )
+    end
+
+    def format_distance(trip_distance)
+      {
+        value: trip_distance.to_s,
+        units: 'km'
+      }
+    end
+
+    def format_fuel_efficiency(fuel_efficiency)
+      {
+        value: fuel_efficiency.to_s,
+        units: 'mpg',
+        of: 'gasoline'
+      }
     end
   end
 end
